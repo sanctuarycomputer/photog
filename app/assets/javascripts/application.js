43d3579e010eb1ffbcbe8e9b2f7be39489340972
Hugh Francis <me@hughfrancis.me>
2015-12-01 15:43:08 -0500

    Implement Magic Rows, closes #107, closes #108

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,29 +1,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require imagesloaded.pkgd.min
-//= require isotope.pkgd
+//= require magic.rows.min
 //= require swiper.jquery
 //= require_tree ./frontend
 //= require fastclick
-$(function(){
-  new FastClick(document.body);
-  var currentView = $('#content').attr('class');
-  window.Photog[currentView+'View'].init();
+
+new FastClick(document.body);
+
+$(window).on('load',function(){
   $content = $('#content');
+  window.Photog[$content.attr('class') + 'View'].init();
   $content.addClass('loaded');
-  
-  // Back Buttons
-  /*
-  var back = $('.trigger-back');
-  back.on('touchend', function() {
-    
-    var referrerIsLocal = document.referrer.indexOf(location.protocol + "//" + location.host) === 0;
-    if (referrerIsLocal) {
-      //window.history.back();
-      window.history.go(-1);
-    } else {
-      window.location.href = location.protocol + "//" + location.host;
-    }
-  });
-  */
 });
